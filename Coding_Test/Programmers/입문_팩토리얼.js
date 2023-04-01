959b40e68c3ac75deb94cd5518735cfd31a73b65
yunajoe <yunajoe@waikorea.com>
2023-04-01 12:45:15 +0900

    다른사람풀이추가

--- Coding_Test/Programmers/입문_팩토리얼.js
+++ Coding_Test/Programmers/입문_팩토리얼.js
@@ -15,3 +15,18 @@ function solution(n) {
     }
     return answer
 }
+
+
+// 다른사람풀이  
+function solution2(n){
+    let ans = 1; 
+    for(let i=1; i<=n; i++){
+        ans *= i
+        if(ans === n){
+            return i; 
+        }
+        if(ans > n){
+            return i-1
+        }  
+    }  
+}
