97f35263f1681edbd091a72b13c22b7f630be18c
yunajoe <yunajoe@waikorea.com>
2023-04-03 18:15:08 +0900

    입문_K의갯수

---
+++ Coding_Test/Programmers/입문_K의개수.js
@@ -0,0 +1,22 @@
+function splitIntoArrayandCount(num) {
+  // x || y  if X can be converted to true, return x otherwise, return y
+  const count = {};
+  const arr = Array.from(String(num));
+  for (let i = 0; i < arr.length; i++) {
+    count[arr[i]] = (count[arr[i]] || 0) + 1;
+  }
+  return count;
+}
+
+function solution(i, j, k) {
+  let cnt = 0;
+  for (let start = i; i <= j; i++) {
+    const splitArr = splitIntoArrayandCount(i);
+    const k_str = k.toString();
+    // splitArr.k_str 하면은 모두 undefined로 나온다.. 왜징?
+    let answer = splitArr[k_str] ? splitArr[k_str] : 0;
+    cnt += answer;
+  }
+  return cnt;
+}
+console.log(solution(10, 50, 5));
