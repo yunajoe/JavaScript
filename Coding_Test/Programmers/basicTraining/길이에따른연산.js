2eff09022d9c3269bbb559cad0036c70b963a605
yunajoe <yunajoe@waikorea.com>
2023-04-24 08:53:17 +0900

    길이에따른연산

--- Coding_Test/Programmers/basicTraining/길이에따른연산.js
+++ Coding_Test/Programmers/basicTraining/길이에따른연산.js
@@ -7,3 +7,7 @@ function solution(num_list) {
     return num_list.reduce((acc, cur) => (acc *= cur), 1);
   }
 }
+
+// sol2
+const solution = (n) =>
+  n.reduce((a, v) => (n.length > 10 ? a + v : a * v), n.length > 10 ? 0 : 1);
