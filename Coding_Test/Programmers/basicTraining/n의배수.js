62b354ddf2f92d0017cca3393449919b43b01c9c
yunajoe <yunajoe@waikorea.com>
2023-04-23 09:51:01 +0900

    n의배수

--- Coding_Test/Programmers/basicTraining/n의배수.js
+++ Coding_Test/Programmers/basicTraining/n의배수.js
@@ -1,5 +1,10 @@
 function solution(num, n) {
-  // num에서 n을 나누었을때 0이면은 (false일때 값이면은) >> 1
-  // 0이 아닐때 (true일때) >> 0
+  return num % n === 0 ? 1 : 0;
+}
+
+function solution(num, n) {
+  // 삼항연산자에서 ? 바로 다음은 true값 : 다음은 false값
+  // 나눴을때 나머지가 0이면은 0은 false이기 때문에 ":" 옆에 해당 return값 넣어주기
+  // 나머지가 0이 아닌 값은 true이기 때문에 "?" 옆에 해당 return값 넣어주기
   return num % n ? 0 : 1;
 }
