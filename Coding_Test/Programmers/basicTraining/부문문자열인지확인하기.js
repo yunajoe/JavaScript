76585118688325af48d8d17617bf460e4f887e23
yunajoe <yunajoe@waikorea.com>
2023-04-23 19:44:57 +0900

    부문문자열인지확인하기

---
+++ Coding_Test/Programmers/basicTraining/부문문자열인지확인하기.js
@@ -0,0 +1,8 @@
+function solution(my_string, target) {
+  return my_string.includes(target) ? 1 : 0;
+}
+
+// 화살표함수
+const solution = (my_string, target) => {
+  return my_string.includes(target) ? 1 : 0;
+};
