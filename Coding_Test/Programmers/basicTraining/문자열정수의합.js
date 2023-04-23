c24388c3775d4bae40f5b7a791f6ed2fd8ba5d4e
yunajoe <yunajoe@waikorea.com>
2023-04-23 11:13:05 +0900

    문자열정수의합

--- Coding_Test/Programmers/basicTraining/문자열정수의합.js
+++ Coding_Test/Programmers/basicTraining/문자열정수의합.js
@@ -5,3 +5,8 @@ function solution(num_str) {
   const arr = [...num_str];
   return arr.reduce((acc, num) => (acc += Number(num)), 0);
 }
+
+// 화살표 함수 사용
+// 화살표 함수의 경우, 함수 바디 내부의 문장이 한 줄인 경우, 중괄호({})와 return 키워드를 생략할 수 있다
+const solution = (num_str) =>
+  [...num_str].reduce((acc, curr) => acc + Number(curr), 0);
