cd4c0f306d0e53ace5ccbe6a00672279f8253c12
yunajoe <yunajoe@waikorea.com>
2023-04-25 19:12:34 +0900

    다른풀이추가

--- Coding_Test/Programmers/basicTraining/홀수vs짝수.js
+++ Coding_Test/Programmers/basicTraining/홀수vs짝수.js
@@ -1,23 +1,25 @@
-// // num_list에 index를 짝수와 홀수로 구분한다
-// // 각각 map함수로 돌리면서.. 합을 구하고
-// function solution(num_list) {
-//   const even_list = num_list
-//     .filter((num, idx) => {
-//       return idx % 2 === 0;
-//     })
-//     .reduce((acc, cur) => {
-//       return (acc += cur);
-//     }, 0);
+// num_list에 index를 짝수와 홀수로 구분한다
+// 각각 map함수로 돌리면서.. 합을 구하고
+function solution(num_list) {
+  const even_list = num_list
+    .filter((num, idx) => {
+      return idx % 2 === 0;
+    })
+    .reduce((acc, cur) => {
+      return (acc += cur);
+    }, 0);
 
-//   const odd_list = num_list
-//     .filter((num, idx) => {
-//       return idx % 2 !== 0;
-//     })
-//     .reduce((acc, cur) => {
-//       return (acc += cur);
-//     }, 0);
-//   return odd_list > even_list ? odd_list : even_list;
-// }
+  const odd_list = num_list
+    .filter((num, idx) => {
+      return idx % 2 !== 0;
+    })
+    .reduce((acc, cur) => {
+      return (acc += cur);
+    }, 0);
+  return odd_list > even_list ? odd_list : even_list;
+}
+
+// sol2
 function solution(num_list) {
   let even = 0;
   let odd = 0;
