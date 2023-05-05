d82403d487b15776411dbdf6cc35fc6d102e2dc3
yunajoe <yunajoe@waikorea.com>
2023-05-05 22:37:28 +0900

    다른풀이추가

--- Coding_Test/Programmers/basicTraining/공백으로구분하기2.js
+++ Coding_Test/Programmers/basicTraining/공백으로구분하기2.js
@@ -2,3 +2,11 @@ function solution(my_string) {
   var answer = [];
   return my_string.split(" ").filter((str) => str !== "");
 }
+
+// sol2
+// filter 메서드는 truthy한 값만을 return한다
+// 빈문자열, null, undefined, 0 과 같은 falsy한 값은 return x
+function solution(my_string) {
+  var answer = [];
+  return my_string.split(" ").filter((str) => str);
+}
