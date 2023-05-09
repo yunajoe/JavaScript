0a9e2c1d50c13f1a42539de67ef835559c201528
yunajoe <yunajoe@waikorea.com>
2023-05-09 18:52:00 +0900

    배열에서문자열대소문자변환하기

--- Coding_Test/Programmers/basicTraining/배열에서문자열대소문자변환하기.js
+++ Coding_Test/Programmers/basicTraining/배열에서문자열대소문자변환하기.js
@@ -1,5 +1,11 @@
+// function solution(strArr) {
+//   return strArr.map((str, idx) => {
+//     return idx % 2 === 0 ? str.toLowerCase() : str.toUpperCase();
+//   });
+// }
+
 function solution(strArr) {
-  return strArr.map((str, idx) => {
-    return idx % 2 === 0 ? str.toLowerCase() : str.toUpperCase();
-  });
+  return strArr.map((v, idx) =>
+    idx % 2 === 0 ? v.toLowerCase() : v.toUpperCase()
+  );
 }
