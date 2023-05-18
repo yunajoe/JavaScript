5fcb32cdea36b92ae83b691d0b1a2f10009a310e
yunajoe <yunajoe@gmail.com>
2023-05-19 08:05:29 +0900

    다른풀이추가

--- Coding_Test/Programmers/basicTraining/수조작하기2.js
+++ Coding_Test/Programmers/basicTraining/수조작하기2.js
@@ -26,3 +26,79 @@ function solution(numLog) {
   }
   return strArr;
 }
+
+// sol2
+// switch문으로 사용하기
+function solution(numLog) {
+  let answer = [];
+  let answerStr = "";
+  for (let i = numLog.length - 1; i > 0; i--) {
+    let result = numLog[i] - numLog[i - 1];
+    answer.push(result);
+  }
+  answer.reverse();
+  answer.map((ele) => {
+    const num = ele;
+    switch (num) {
+      case 1:
+        answerStr += "w";
+        break;
+      case -1:
+        answerStr += "s";
+        break;
+      case 10:
+        answerStr += "d";
+        break;
+      case -10:
+        answerStr += "a";
+        break;
+    }
+  });
+  return answerStr;
+}
+
+// objec으로 사용하기
+function solution(numLog) {
+  var answer = "";
+  const order = {
+    w: 1,
+    s: -1,
+    d: 10,
+    a: -10,
+  };
+
+  for (let i = 1; i < numLog.length; i++) {
+    const prev = numLog[i - 1];
+    const curr = numLog[i];
+
+    for (let [key, value] of Object.entries(order)) {
+      const res = prev + value;
+
+      if (res === curr) {
+        answer += key;
+        break;
+      }
+    }
+  }
+}
+
+// sol3)
+function solution(numLog) {
+  let answer = "";
+  const obj = {
+    w: 1,
+    s: -1,
+    d: 10,
+    a: -10,
+  };
+  for (let i = 1; i < numLog.length; i++) {
+    const prev = numLog[i - 1]; // 0 ~ 10
+    const curr = numLog[i]; // 1 ~ 11
+    for (let [key, value] of Object.entries(obj)) {
+      if (prev + value === curr) {
+        answer += key;
+      }
+    }
+  }
+  return answer;
+}
