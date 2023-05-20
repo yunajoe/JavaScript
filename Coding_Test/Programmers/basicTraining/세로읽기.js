3b761047ac69982183b0771ccdd85b3464ca234e
yunajoe <yunajoe@gmail.com>
2023-05-21 00:04:09 +0900

    다른솔루션추가

--- Coding_Test/Programmers/basicTraining/세로읽기.js
+++ Coding_Test/Programmers/basicTraining/세로읽기.js
@@ -10,3 +10,11 @@ function solution(my_string, m, c) {
   }
   return answer;
 }
+
+// sol2)
+
+function solution(my_string, m, c) {
+  return Array.from(my_string).reduce((acc, v, i) => {
+    return (i % m) + 1 === c ? (acc += v) : acc;
+  }, "");
+}
