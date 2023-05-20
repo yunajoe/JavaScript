fe42b278b92bdbfd637d48fa8b3c53f405b64c30
yunajoe <yunajoe@gmail.com>
2023-05-20 23:36:53 +0900

    세로읽기

---
+++ Coding_Test/Programmers/basicTraining/세로읽기.js
@@ -0,0 +1,12 @@
+function solution(my_string, m, c) {
+  let answer = "";
+  for (let i = 0; i <= my_string.length; i = i + m) {
+    let str = my_string.slice(i, i + m);
+    if (str.length !== 0) {
+      answer += [...str].reduce((acc, cur, idx) => {
+        return idx === c - 1 ? (acc += cur) : acc;
+      }, "");
+    }
+  }
+  return answer;
+}
