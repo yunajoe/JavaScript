e3cc2b5d42e046ad741673f9d9738e0e95558e15
yunajoe <yunajoe@gmail.com>
2023-08-30 08:46:53 +0900

    치킨쿠폰

--- Coding_Test/Programmers/entry/치킨쿠폰.js
+++ Coding_Test/Programmers/entry/치킨쿠폰.js
@@ -1,3 +1,4 @@
+// first-trial 실패
 function solution(chicken) {
   let serviceChicken = 0;
   let resCouponTotal = 0;
@@ -20,3 +21,39 @@ function solution(chicken) {
 
 // 정확성: 88.1;
 // 합계: 88.1 / 100.0;
+
+//  2nd-trial 성공!
+function solution(chicken) {
+  let serviceChickenCount = 0;
+  let issuedCouponCount = 0;
+  // 쿠폰 10장으로 chicken 1마리
+  let coupon = chicken; // 원시타입의 값은 always deepcopy
+  if (coupon < 10) {
+    return 0;
+  }
+  let serviceChicken = Math.floor(coupon / 10);
+  let issuedCoupon = coupon % 10;
+
+  serviceChickenCount += serviceChicken;
+  issuedCouponCount += issuedCoupon;
+
+  while (serviceChicken !== 0) {
+    issuedCoupon = serviceChicken % 10;
+    serviceChicken = Math.floor(serviceChicken / 10);
+
+    serviceChickenCount += serviceChicken;
+    issuedCouponCount += issuedCoupon;
+  }
+
+  if (issuedCouponCount < 10) {
+    return serviceChickenCount;
+  }
+
+  // 아래는 나머지 쿠폰이 10장이 남았을때 해당하는 경우, 예를 들어 chicken이 1999 경우 result는 222
+  while (issuedCouponCount >= 10) {
+    serviceChickenCount += Math.floor(issuedCouponCount / 10);
+    issuedCouponCount =
+      (issuedCouponCount % 10) + Math.floor(issuedCouponCount / 10);
+  }
+  return serviceChickenCount;
+}
