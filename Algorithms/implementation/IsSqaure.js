88d945f585f18888566ec381dfd2bbe1901e240e
yunajoe <yunajoe@gmail.com>
2023-09-13 08:16:29 +0900

    IsSqaure.js

--- Algorithms/implementation/IsSqaure.js
+++ Algorithms/implementation/IsSqaure.js
@@ -1,13 +1,4 @@
-/*
-if(arr1과 arr2의 길이가 같으면){
-    let newArr1 = arr1의 원소들을 다 제곱하여 새로운 배열로 만든다.
-    새로운 배열의 원소들이랑 arr2의 배열의 원소들이 다 같으면 true
-}
-return false
-단순히 arr1의 원소가 arr2의 원소에만 있다고 확인하명는 X 갯수도 같아야... 
-(return arr1.every((value) => arr2.includes(value));)
-*/
-
+// 나의풀이;
 const compareFunc = (arr1, arr2) => {
   arr1.sort((a, b) => a - b);
   arr2.sort((a, b) => a - b);
@@ -22,6 +13,34 @@ function same(arr1, arr2) {
   return false;
 }
 
-console.log(same([1, 2, 3], [4, 1, 9])); // true
-console.log(same([1, 2, 3], [1, 9])); // false
-console.log(same([1, 2, 1], [4, 1, 4])); // false
+// 해설풀이
+/*
+array들을 object로 만든다.
+object1의 key**2이 obejct2에 있는지 확인 >> 우선, 키값만 확인 , 키가 없으면 return false!
+obejct1 의 object1[key]값이 object[key**2]와 같은지 확인 >> 키에 해당되는 값을 확인
+
+*/
+function solution(arr1, arr2) {
+  let obj1 = {};
+  let obj2 = {};
+  for (let value of arr1) {
+    obj1[value] = (obj1[value] || 0) + 1;
+  }
+  for (let value of arr2) {
+    obj2[value] = (obj2[value] || 0) + 1;
+  }
+
+  for (let key1 in obj1) {
+    let value = key1 ** 2;
+    // if(!value in obj2)
+    if (!Object.keys(obj2).includes(value)) {
+      return false;
+    }
+    if (obj1[value] !== obj2[value]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+solution([1, 1, 2, 4], [1, 1, 4, 16]);
